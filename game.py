ab76e3c17fb968334ae449f214f6b3678f158705
cs3214 <0jj5d2073292w5p@au.com>
2025-07-03 16:28:31 +0900

    Won/lost message added

--- game.py
+++ game.py
@@ -5,10 +5,12 @@ def main():
     print("Rolling dice...")
     d1 = random.randint(1, 6)
     d2 = random.randint(1, 6)
-    total = str(d1 + d2)
+    total = d1 + d2
     print("Die 1:" + str(d1))
     print("Die 2:" + str(d2))
-    print("Total value: " + total)
+    print("Total value: " + str(total))
+    if total > 7: print("You won")
+    else: print("You lose")
 
 
 if __name__ == "__main__":
