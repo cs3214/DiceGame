c708a2ce9786dd2b618d2b70780ba630ae9a488a
cs3214 <0jj5d2073292w5p@au.com>
2025-07-03 16:05:32 +0900

    first

---
+++ game.py
@@ -0,0 +1,15 @@
+import random
+
+def main():
+
+    print("Rolling dice...")
+    d1 = random.randint(1, 6)
+    d2 = random.randint(1, 6)
+    total = str(d1 + d2)
+    print("Die 1:" + str(d1))
+    print("Die 2:" + str(d2))
+    print("Total value: " + total)
+
+
+if __name__ == "__main__":
+  main()
